464711446bf6170af5e2a4977cc7908ab22e0663
Jennifer Stephens <xxjennandtonicxx@gmail.com>
2023-04-23 17:51:35 -0700

    import components

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+import ActivityList from './components/ActivityList'
+import AddActivity from './components/AddActivity'
+import Login from './components/Login'
+import SignUp from './components/SignUp'
+import ActivityDetail from './components/ActivityDetail'
+
+function App() {
+
+
+return (
+    <div className='App'>
+        <Login/>
+    </div>
+)
+}
