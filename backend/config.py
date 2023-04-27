b426483048e82c9063c2c1c4982dc71ad61ee780
Jennifer Stephens <xxjennandtonicxx@gmail.com>
2023-04-27 12:00:05 -0700

    remove config.py file

--- backend/config.py
+++
@@ -1,6 +0,0 @@
-# password = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR4dGp3YXRoanF6c21lYWh5eHJ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODIxOTA2NTAsImV4cCI6MTk5Nzc2NjY1MH0.hWcNKYcDY3crJxTWc_0CAPc7-2BMDI3cVbjED6HMsNc'
-# SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Mile-st0n3!3@db.dxtjwathjqzsmeahyxrw.supabase.co:5432/postgres'
-
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-# postgresql://postgres:[YOUR-PASSWORD]@db.dxtjwathjqzsmeahyxrw.supabase.co:5432/postgres
